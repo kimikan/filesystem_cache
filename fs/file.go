4e36ce8d066b5fab9ac3fa53273feaa3650bb21c
Kimi Kan <27750364+kimikan@users.noreply.github.com>
2018-07-02 10:41:49 +0800

    Create file.go

---
+++ fs/file.go
@@ -0,0 +1,141 @@
+package fs
+
+import (
+	"errors"
+	"io"
+	"os"
+	"sort"
+	"strings"
+	"time"
+)
+
+type memFileInfo struct {
+	content []byte
+	name    string
+	size    int64
+	mode    os.FileMode
+	modTime time.Time
+	isDir   bool
+	path    string
+	fs      *memFileSystem
+}
+
+func (fi *memFileInfo) Name() string {
+	return fi.name
+}
+
+func (fi *memFileInfo) Size() int64 {
+	return fi.size
+}
+
+func (fi *memFileInfo) Mode() os.FileMode {
+	return fi.mode
+}
+
+func (fi *memFileInfo) ModTime() time.Time {
+	return fi.modTime
+}
+
+func (fi *memFileInfo) IsDir() bool {
+	return fi.isDir
+}
+
+func (fi *memFileInfo) Sys() interface{} {
+	return nil
+}
+
+type fileInfoSorter struct {
+	infos []os.FileInfo
+}
+
+func (s *fileInfoSorter) Len() int {
+	return len(s.infos)
+}
+
+func (s *fileInfoSorter) Swap(i, j int) {
+	s.infos[i], s.infos[j] = s.infos[j], s.infos[i]
+}
+
+func (s *fileInfoSorter) Less(i, j int) bool {
+	return s.infos[i].Name() < s.infos[j].Name()
+}
+
+///start file definition
+type memFile struct {
+	fi        *memFileInfo
+	offset    int64
+	dirOffset int
+}
+
+func (f *memFile) Close() error {
+	return nil
+}
+
+func (f *memFile) Stat() (os.FileInfo, error) {
+	return f.fi, nil
+}
+
+func (f *memFile) Readdir(count int) ([]os.FileInfo, error) {
+	infos := []os.FileInfo{}
+	prefix := f.fi.path
+	skip := 0
+	for path, fi := range f.fi.fs.cache {
+		if strings.HasPrefix(path, prefix) || prefix == "." {
+			if len(path) == len(prefix) {
+				continue // Do not return the current directory.
+			}
+			if strings.LastIndex(path, "/") > len(prefix) {
+				continue // Do not return files from sub directories.
+			}
+
+			if skip < f.dirOffset {
+				skip++
+				continue
+			}
+			infos = append(infos, fi)
+			if len(infos) == count {
+				break
+			}
+		}
+	}
+	f.dirOffset += len(infos)
+
+	// Sort the infos by name and return.
+	fis := &fileInfoSorter{
+		infos: infos,
+	}
+	sort.Sort(fis)
+	return infos, nil
+}
+
+func (f *memFile) Read(p []byte) (n int, err error) {
+	if len(f.fi.content)-int(f.offset) >= len(p) {
+		n = len(p)
+	} else {
+		n = len(f.fi.content) - int(f.offset)
+		err = io.EOF
+	}
+	copy(p, f.fi.content[f.offset:f.offset+int64(n)])
+	f.offset += int64(n)
+	return
+}
+
+var errWhence = errors.New("Seek: invalid whence")
+var errOffset = errors.New("Seek: invalid offset")
+
+func (f *memFile) Seek(offset int64, whence int) (ret int64, err error) {
+	switch whence {
+	default:
+		return 0, errWhence
+	case os.SEEK_SET:
+	case os.SEEK_CUR:
+		offset += f.offset
+	case os.SEEK_END:
+		offset += int64(len(f.fi.content))
+	}
+	if offset < 0 || int(offset) > len(f.fi.content) {
+		return 0, errOffset
+	}
+	f.offset = offset
+	return f.offset, nil
+}
